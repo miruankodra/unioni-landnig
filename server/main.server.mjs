0197d0a0dde4e5352d8dcf2ac6e493a0f2a0be0e
miruan kodra <miruankodra@gmail.com>
2024-09-11 21:55:49 +0200

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3LFOMJDR.mjs";import"./chunk-XKB6CPTT.mjs";import"./chunk-R5OQKRBB.mjs";import"./chunk-HNOED6OP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
